6f49fddc6f33f22270d20097d92226f2f517132e
drg123321123 <idragos191@gmail.com>
2025-09-21 13:51:35 +0300

    first commit

---
+++ deployment/nginx/updatemsg/update.js
@@ -0,0 +1,6 @@
+// update message
+// show this page when updating
+// after 10 seconds, redirect to max verstappen video
+setTimeout(function() {
+    window.location.href = "https://youtu.be/mS41bHNw1WI";
+}, 10000);
